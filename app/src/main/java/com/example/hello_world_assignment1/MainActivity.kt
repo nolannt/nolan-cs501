9ec3c66aaf2f7a98b015da25b05488b673b9b3db
nolanturi <nturi@bu.edu>
2023-09-11 22:17:15 -0400

    commit

--- app/src/main/java/com/example/hello_world_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/hello_world_assignment1/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.hello_world_assignment1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var myName: String = "Nolan"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "Hi my name is $myName"
     }
 }
